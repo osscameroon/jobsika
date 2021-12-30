03fd4953b9a101aff428ae61604eb121bad37971
elhmn <nleme@live.fr>
2021-12-31 00:31:31 +0100

    feat: change company to company_id column in salaries table

--- backend/pkg/models/v1beta/salaries.go
+++ backend/pkg/models/v1beta/salaries.go
@@ -18,11 +18,9 @@ type Salary struct {
 	Salary int64  `json:"salary" gorm:"column:salary"`
 	//Seniority can be set to the values bellow
 	//Intern, Entry-level, Mid-level, Senior, Above Senior level, Executive
-	Seniority string `json:"seniority" gorm:"column:seniority"`
-	City      string `json:"city" gorm:"column:city"`
-	//The Company field should be shown only if there are more than 4 salary
-	//records with the same company
-	Company   string    `json:"company" gorm:"column:company"`
+	Seniority string    `json:"seniority" gorm:"column:seniority"`
+	City      string    `json:"city" gorm:"column:city"`
+	CompanyID int64     `json:"company_id" gorm:"column:company_id"`
 	CreatedAt time.Time `json:"createdat" gorm:"column:createdat"`
 	UpdatedAt time.Time `json:"updatedat" gorm:"column:updatedat"`
 }
