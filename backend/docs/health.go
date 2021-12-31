caa8a102f7ad10e728b32e3782ddec7945277452
elhmn <nleme@live.fr>
2021-12-31 04:25:21 +0100

    feat: add health endpoint to the docs

---
+++ backend/docs/health.go
@@ -0,0 +1,16 @@
+package docs
+
+// swagger:route GET /health health idOfHealth
+// Companies returns health endpoint
+// responses:
+//   200: healthResponse
+
+// This text will appear as description of your response body.
+// swagger:response healthResponse
+type HealthResponseWrapper struct {
+	// in:body
+	Body struct {
+		//Example: all good !
+		Message string `json:"message"`
+	}
+}
