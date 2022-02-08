7ad579cea1a2f6dfd452bd9eed8018d4f26c3904
elhmn <nleme@live.fr>
2022-02-08 21:37:04 +0100

    feat: sort list of companies

--- backend/internal/storage/companies.go
+++ backend/internal/storage/companies.go
@@ -5,7 +5,7 @@ import "github.com/elhmn/camerdevs/pkg/models/v1beta"
 //GetCompanies get companies
 func (db DB) GetCompanies() ([]v1beta.Company, error) {
 	companies := []v1beta.Company{}
-	ret := db.c.Find(&companies)
+	ret := db.c.Table("companies").Order("name").Find(&companies)
 	if ret.Error != nil {
 		return companies, ret.Error
 	}
