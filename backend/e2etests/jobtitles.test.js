344656c95369e4874075f5ac135ec2658e77db47
elhmn <nleme@live.fr>
2022-02-08 19:11:50 +0100

    feat: add jobtitles e2etest

---
+++ backend/e2etests/jobtitles.test.js
@@ -0,0 +1,24 @@
+import { expect } from "chai";
+import request from "supertest";
+import dotenv from "dotenv";
+
+dotenv.config();
+const apiHost = process.env.API_HOST;
+const endpoint = "jobtitles";
+
+describe(`${endpoint}`, function () {
+  describe("GET", function () {
+    it("return a list of jobtitles", async function () {
+      return request(apiHost)
+        .get(endpoint)
+        .send()
+        .expect(200)
+        .expect("Content-Type", "application/json; charset=utf-8")
+        .then((res) => {
+          expect(JSON.stringify(res.body[0])).equal(
+            '"Account Coordinator"'
+          );
+        });
+    });
+  });
+});
