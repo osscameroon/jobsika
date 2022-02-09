f928e3411556e04e4d654a0dbe816c447589b320
Asam237 <abbasalimokolo@gmail.com>
2022-02-09 19:12:01 +0100

    AverageRating: show error text on failure

--- frontend/components/Average.vue
+++ frontend/components/Average.vue
@@ -1,50 +1,59 @@
 <template>
-  <div
-    style="background: #235365; font-family: 'Inter', sans-serif"
-    class="site__average py-2 md:py-4 opacity-60 rounded-md my-3"
-  >
-    <div class="flex flex-row justify-between items-center">
-      <div class="site__average__title w-full md:w-1/6 ml-10">
-        <p
-          class="text-white text-xs lg:text-sm font-medium"
-          style="font-family: 'Inter', sans-serif"
-        >
-          {{ title }}
-        </p>
-      </div>
-      <div
-        class="
-          site__average__content
-          w-full
-          flex flex-col
-          md:flex-row
-          items-center
-          md:w-1/2
-          ml-10
-          mr-2
-          md:ml-0 md:mr-0
-        "
-      >
-        <p
-          class="
-            text-white text-xs
-            lg:text-sm
-            font-medium
-            md:ml-10
-            lg:ml-20
-            text-center
-          "
-          style="font-family: 'Inter', sans-serif"
-        >
-          {{ salaryRating.salary }} FCFA
-        </p>
-        <div class="flex pt-1 md:pt-0 ml-0 md:ml-24 md:mr-10">
-          <div
-            v-for="(item, index) in salaryRating.rating"
-            :key="index"
-            class="flex"
+  <div>
+    <div class="text-center py-4 md:py-10 font-bold text-sm" v-if="errored">
+      <p class="text-red-500">
+        We're sorry, we're not able to retrieve this information at the moment,
+        please try back later
+      </p>
+    </div>
+    <div
+      v-else
+      style="background: #235365; font-family: 'Inter', sans-serif"
+      class="site__average py-2 md:py-4 opacity-60 rounded-md my-3"
+    >
+      <div class="flex flex-row justify-between items-center">
+        <div class="site__average__title w-full md:w-1/6 ml-10">
+          <p
+            class="text-white text-xs lg:text-sm font-medium"
+            style="font-family: 'Inter', sans-serif"
           >
-            <img class="w-4 h-4 mr-1" :src="starsPicture" />
+            {{ title }}
+          </p>
+        </div>
+        <div
+          class="
+            site__average__content
+            w-full
+            flex flex-col
+            md:flex-row
+            items-center
+            md:w-1/2
+            ml-10
+            mr-2
+            md:ml-0 md:mr-0
+          "
+        >
+          <p
+            class="
+              text-white text-xs
+              lg:text-sm
+              font-medium
+              md:ml-10
+              lg:ml-20
+              text-center
+            "
+            style="font-family: 'Inter', sans-serif"
+          >
+            {{ salaryRating.salary }} FCFA
+          </p>
+          <div class="flex pt-1 md:pt-0 ml-0 md:ml-24 md:mr-10">
+            <div
+              v-for="(item, index) in salaryRating.rating"
+              :key="index"
+              class="flex"
+            >
+              <img class="w-4 h-4 mr-1" :src="starsPicture" />
+            </div>
           </div>
         </div>
       </div>
@@ -62,13 +71,16 @@ export default Vue.extend({
     return {
       starsPicture: require('../assets/star.png'),
       salaryRating: '',
+      loading: true,
+      errored: false,
     }
   },
   async created() {
     try {
       this.salaryRating = (await axios.get(BASE_URL + '/average-rating')).data
-    } catch (e) {
-      console.log(e)
+    } catch (err) {
+      console.log(err)
+      this.errored = true
     }
   },
 })
