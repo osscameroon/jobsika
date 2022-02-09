9abf50baf4b17c4165ea00bda7b9f6be986043ec
elhmn <nleme@live.fr>
2022-02-09 17:14:14 +0100

    feat: add creation date to ratings

--- backend/pkg/models/v1beta/ratings.go
+++ backend/pkg/models/v1beta/ratings.go
@@ -1,5 +1,7 @@
 package v1beta
 
+import "time"
+
 //RatingQuery is a Rating query structure
 type RatingQuery struct {
 	ID int64 `form:"rating_id"`
@@ -8,17 +10,18 @@ type RatingQuery struct {
 //Rating defines the rating structure
 //This structure combines a salary and a company rating entry
 type Rating struct {
-	SalaryID        int64  `json:"salary_id" gorm:"column:salary_id"`
-	CompanyID       int64  `json:"company_id" gorm:"column:company_id"`
-	CompanyRatingID int64  `json:"company_rating_id" gorm:"column:company_rating_id"`
-	Rating          int64  `json:"rating" gorm:"column:rating"`
-	Salary          int64  `json:"salary" gorm:"column:salary"`
-	CompanyName     string `json:"company_name" gorm:"column:company_name"`
-	Seniority       string `json:"seniority" gorm:"column:seniority"`
-	Comment         string `json:"comment" gorm:"column:comment"`
-	JobTitle        string `json:"job_title" gorm:"column:job_title"`
-	Country         string `json:"country" gorm:"column:country"`
-	City            string `json:"city" gorm:"column:city"`
+	SalaryID        int64     `json:"salary_id" gorm:"column:salary_id"`
+	CompanyID       int64     `json:"company_id" gorm:"column:company_id"`
+	CompanyRatingID int64     `json:"company_rating_id" gorm:"column:company_rating_id"`
+	Rating          int64     `json:"rating" gorm:"column:rating"`
+	Salary          int64     `json:"salary" gorm:"column:salary"`
+	CompanyName     string    `json:"company_name" gorm:"column:company_name"`
+	Seniority       string    `json:"seniority" gorm:"column:seniority"`
+	Comment         string    `json:"comment" gorm:"column:comment"`
+	JobTitle        string    `json:"job_title" gorm:"column:job_title"`
+	Country         string    `json:"country" gorm:"column:country"`
+	City            string    `json:"city" gorm:"column:city"`
+	CreatedAt       time.Time `json:"createdat" gorm:"column:createdat"`
 }
 
 //AverageRating defines the average rating structure
