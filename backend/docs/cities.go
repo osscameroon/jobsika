9faff2afa3b75e002f329eca08699dbefa6c95e6
elhmn <nleme@live.fr>
2022-02-12 13:00:57 +0100

    feat: add cities endpoint docs to swagger

---
+++ backend/docs/cities.go
@@ -0,0 +1,14 @@
+package docs
+
+// swagger:route GET /cities cities idOfCities
+// Companies returns cities endpoint
+// responses:
+//   200: citiesResponse
+
+// This text will appear as description of your response body.
+// swagger:response citiesResponse
+type CitiesResponseWrapper struct {
+	// in:body
+	//Example: ["Douala","Yaoundé","Garoua","Bamenda","Maroua","Nkongsamba","Bafoussam","Ngaoundéré"]
+	Body []string
+}
