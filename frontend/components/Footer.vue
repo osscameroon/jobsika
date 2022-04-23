305c6bae384b21d335445a855fce15e2b43e42d1
Asam237 <abbasalimokolo@gmail.com>
2022-04-23 20:51:00 +0100

    OSS Cameroon copyright added

---
+++ frontend/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer
+    class="site__footer mt-10 md:mt-28 flex flex-col md:flex-row items-center justify-center"
+  >
+    <img :src="github" alt="logo" class="site__footer-img w-8 h-8" />
+    <p class="site__footer-copyrigth text-xs md:text-sm md:ml-7 mt-3 md:mt-0">
+      © 2022 OSS Cameroon, All rights reserved.
+    </p>
+  </footer>
+</template>
+<script>
+export default {
+  name: 'FooterComponent',
+  data() {
+    return {
+      github: require('../assets/github.png'),
+    }
+  },
+}
+</script>
