e5f3e59c93f495d92ca224131e24f7f5f638a58b
Asam237 <abbasalimokolo@gmail.com>
2022-04-23 21:47:24 +0100

    add github link

--- frontend/components/Footer.vue
+++ frontend/components/Footer.vue
@@ -2,7 +2,9 @@
   <footer
     class="site__footer mt-10 md:mt-28 flex flex-col md:flex-row items-center justify-center"
   >
-    <img :src="github" alt="logo" class="site__footer-img w-8 h-8" />
+    <a href="https://github.com/osscameroon/camerdevs">
+      <img :src="github" alt="logo" class="site__footer-img w-8 h-8" />
+    </a>
     <p class="site__footer-copyrigth text-xs md:text-sm md:ml-7 mt-3 md:mt-0">
       © 2022 OSS Cameroon, All rights reserved.
     </p>
