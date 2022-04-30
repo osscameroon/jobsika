b30a2a5a2a71265fb60a5375d3a2ac6c7561fa68
Asam237 <abbasalimokolo@gmail.com>
2022-04-30 11:57:16 +0100

    change nuxt-link to a href

--- frontend/layouts/app.vue
+++ frontend/layouts/app.vue
@@ -4,13 +4,13 @@
       class="site__logo bg-blueDark flex justify-center items-center md:justify-start py-3 md:py-2 z-50 sticky top-0 left-0 shadow-md"
     >
       <div class="md:container md:mx-auto md:w-10/12">
-        <nuxt-link to="/" class="cursor-pointer">
+        <a href="/" class="cursor-pointer">
           <img
             :src="logo"
             alt="logo"
             class="site__logo-pic w-32 h-12 md:w-36 md:h-14"
           />
-        </nuxt-link>
+        </a>
       </div>
     </header>
     <Nuxt />
