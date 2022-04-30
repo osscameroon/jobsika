cd2e3b7c85905a0cd86b85159524d7c3236e2580
Aser <aserdipanda@gmail.com>
2022-04-30 15:26:36 +0100

    update tests

--- backend/e2etests/seniority.test.js
+++ backend/e2etests/seniority.test.js
@@ -15,9 +15,7 @@ describe(`${endpoint}`, function () {
         .expect(200)
         .expect("Content-Type", "application/json; charset=utf-8")
         .then((res) => {
-          expect(JSON.stringify(res.body)).equal(
-            '["Douala","Yaoundé","Garoua","Bamenda","Maroua","Nkongsamba","Bafoussam","Ngaoundéré","Bertoua","Loum","Kumba","Edéa","Kumbo","Foumban","Mbouda","Dschang","Limbé","Ebolowa","Kousséri","Guider","Meiganga","Yagoua","Mbalmayo","Bafang","Tiko","Bafia","Wum","Kribi","Buea","Sangmélima","Foumbot","Bangangté","Batouri","Banyo","Nkambé","Bali","Mbanga","Mokolo","Melong","Manjo","Garoua-Boulaï","Mora","Kaélé","Tibati","Ndop","Akonolinga","Eséka","Mamfé","Obala","Muyuka","Nanga-Eboko","Abong-Mbang","Fundong","Nkoteng","Fontem","Mbandjock","Touboro","Ngaoundal","Yokadouma","Pitoa","Tombel","Kékem","Magba","Bélabo","Tonga","Maga","Koutaba","Blangoua","Guidiguis","Bogo","Batibo","Yabassi","Figuil","Makénéné","Gazawa","Tcholliré"]'
-          );
+            expect(JSON.stringify(res.body[0])).equal('"Abong-Mbang"')
         });
     });
   });
