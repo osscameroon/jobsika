33ed2589d59005e468d66cccaf6c1ed20bf49b22
Aser Dipanda <aserdipanda@gmail.com>
2023-03-04 14:07:18 +0100

    update fields

--- backend/pkg/models/v1beta/job_offers.go
+++ backend/pkg/models/v1beta/job_offers.go
@@ -13,30 +13,44 @@ type JobOffer struct {
 	CreatedAt time.Time `json:"createdat" gorm:"column:createdat"`
 	UpdatedAt time.Time `json:"updatedat" gorm:"column:updatedat"`
 
-	Email             string `json:"email" gorm:"column:email"`
 	CompanyName       string `json:"company_name" gorm:"column:company_name"`
+	CompanyEmail      string `json:"company_email" gorm:"column:company_email"`
 	TitleID           int64  `json:"title" gorm:"column:title_id"`
 	IsRemote          bool   `json:"is_remote" gorm:"column:is_remote"`
+	Location          string `json:"location" gorm:"column:location"`
+	Department        string `json:"department" gorm:"column:department"`
+	SalaryRangeMin    int64  `json:"salary_range_min" gorm:"column:salary_range_min"`
+	SalaryRangeMax    int64  `json:"salary_range_max" gorm:"column:salary_range_max"`
 	Description       string `json:"description" gorm:"column:description"`
+	Benefits          string `json:"benefits" gorm:"column:benefits"`
 	HowToApply        string `json:"how_to_apply" gorm:"column:how_to_apply"`
 	ApplyUrl          string `json:"apply_url" gorm:"column:apply_url"`
 	ApplyEmailAddress string `json:"apply_email_address" gorm:"column:apply_email_address"`
+	ApplyPhoneNumber  string `json:"apply_phone_number" gorm:"column:apply_phone_number"`
+	Tags              string `json:"tags" gorm:"column:tags"`
 }
 
 // OfferPostQuery defines the body object used to create a new jb offer on a POST query
 type OfferPostQuery struct {
 	Email             string `json:"email"`
 	CompanyName       string `json:"company_name"`
+	CompanyEmail      string `json:"company_email"`
 	JobTitle          string `json:"job_title"`
 	IsRemote          bool   `json:"is_remote"`
+	Location          string `json:"location"`
+	Department        string `json:"department"`
+	SalaryRangeMin    int64  `json:"salary_range_min"`
+	SalaryRangeMax    int64  `json:"salary_range_max"`
 	Description       string `json:"description"`
 	HowToApply        string `json:"how_to_apply"`
 	ApplyUrl          string `json:"apply_url"`
 	ApplyEmailAddress string `json:"apply_email_address"`
+	ApplyPhoneNumber  string `json:"apply_phone_number"`
+	Tags              string `json:"tags"`
 }
 
-// Validate check if the mandatory fields are filled
-func (r OfferPostQuery) Validate() error {
+// Validate check if the mandatory fields are filled, and make some changes in the tags field
+func (r *OfferPostQuery) Validate() error {
 	if strings.TrimSpace(r.Email) == "" {
 		return errors.New("email is mandatory")
 	}
@@ -58,7 +72,7 @@ func (r OfferPostQuery) Validate() error {
 		return errors.New("job description is mandatory")
 	}
 
-	if strings.TrimSpace(r.ApplyUrl) == "" && strings.TrimSpace(r.ApplyEmailAddress) == "" {
+	if strings.TrimSpace(r.ApplyUrl) == "" && strings.TrimSpace(r.ApplyEmailAddress) == "" && strings.TrimSpace(r.ApplyPhoneNumber) == "" {
 		return errors.New("apply url or apply email address is mandatory")
 	}
 
@@ -66,6 +80,52 @@ func (r OfferPostQuery) Validate() error {
 		if !emailRegex.MatchString(r.ApplyEmailAddress) {
 			return errors.New("apply email address is not a valid email address")
 		}
+	}
+
+	if strings.TrimSpace(r.ApplyPhoneNumber) != "" {
+		phoneRegex := regexp.MustCompile(`^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$`)
+		if !phoneRegex.MatchString(r.ApplyPhoneNumber) {
+			return errors.New("apply phone number is not a valid phone number")
+		}
+	}
+
+	if r.SalaryRangeMin == 0 && r.SalaryRangeMax == 0 {
+		return errors.New("salary range is mandatory")
+	}
+
+	if r.SalaryRangeMin > r.SalaryRangeMax {
+		return errors.New("min salary is higher than max salary")
+	}
+
+	if strings.TrimSpace(r.Location) == "" && !r.IsRemote {
+		return errors.New("location is mandatory when is not remote")
+	}
+
+	// tag should be separated by a comma, each tag should be at least 1 characters long with no space, tags with space will be ignored, tags sould be unique
+	if strings.TrimSpace(r.Tags) != "" {
+		tags := strings.Split(r.Tags, ",")
+		finalTags := ""
+		finalTagsMap := make(map[string]struct{})
+		for _, tag := range tags {
+			if strings.TrimSpace(tag) == "" {
+				continue
+			}
+
+			if _, tagIsDuplicate := finalTagsMap[tag]; tagIsDuplicate {
+				continue
+			}
+
+			if finalTags != "" {
+				finalTags += ","
+			}
+
+			finalTags += tag
+
+			finalTagsMap[tag] = struct{}{}
+
+		}
+
+		r.Tags = finalTags
 	}
 
 	return nil
@@ -76,14 +136,18 @@ type JobOfferPresenter struct {
 	CreatedAt time.Time `json:"createdat" gorm:"column:createdat"`
 	UpdatedAt time.Time `json:"updatedat" gorm:"column:updatedat"`
 
-	Email             string `json:"email"`
 	CompanyName       string `json:"company_name"`
 	JobTitle          string `json:"job_title"`
 	IsRemote          bool   `json:"is_remote"`
+	Location          string `json:"location"`
+	Department        string `json:"department"`
+	SalaryRange       string `json:"salary_range"`
 	Description       string `json:"description"`
+	Benefits          string `json:"benefits"`
 	HowToApply        string `json:"how_to_apply"`
 	ApplyUrl          string `json:"apply_url"`
 	ApplyEmailAddress string `json:"apply_email_address"`
+	ApplyPhoneNumber  string `json:"apply_phone_number"`
 }
 
 type JobOffersResponse struct {
@@ -99,6 +163,6 @@ type GetJobOffersQuery struct {
 	Limit    string
 	JobTitle string
 	Company  string
-	City     string
+	Location string
 	IsRemote string
 }
