6618a88783652e48abda7a0a2c7364ca34ce7515
Aser Dipanda <aserdipanda@gmail.com>
2023-04-01 11:05:31 +0200

    revert first migratin file

--- backend/migrations/20211019221200_init_db.sql
+++ backend/migrations/20211019221200_init_db.sql
@@ -44,28 +44,6 @@ CREATE TABLE IF NOT EXISTS cities (
     updatedat DATE
 );
 
-CREATE TABLE IF NOT EXISTS job_offers (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    company_name VARCHAR(255),
-    company_email VARCHAR(255),
-    title_id INTEGER NOT NULL,
-    is_remote BOOLEAN,
-    city VARCHAR(255),
-    country VARCHAR(255),
-    department VARCHAR(255),
-    salary_range_min BIGSERIAL,
-    salary_range_max BIGSERIAL,
-    description VARCHAR(5000),
-    benefits VARCHAR(1000),
-    how_to_apply VARCHAR(1000),
-    application_url VARCHAR(255),
-    application_email_address VARCHAR(255),
-    application_phone_number VARCHAR(255),
-    tags VARCHAR(255),
-
-    createdat DATE,
-    updatedat DATE
-);
 
 -- +goose StatementEnd
 -- +goose Down
@@ -75,5 +53,4 @@ DROP TABLE IF EXISTS companies;
 DROP TABLE IF EXISTS jobtitles;
 DROP TABLE IF EXISTS cities;
 DROP TABLE IF EXISTS company_ratings;
-DROP TABLE IF EXISTS job_offers;
 -- +goose StatementEnd
