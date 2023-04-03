07aba1525d0b41431926f5c901f9bdb6bcd595e4
Alex <mboutchouangalex+github@gmail.com>
2023-04-03 23:08:39 +0200

    Add Props to More component to make it reusable

--- frontend/components/More.vue
+++ frontend/components/More.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="bg-white shadow-md h-14 py-2 flex justify-center items-center border-t-2">
-        <NuxtLink to="#" class="font-normal text-lg ext-gray-700 flex items-center">
-            See more jobs <span class="ml-4"><img class="w-auto h-4" :src="arrow" /></span>
+        <NuxtLink :to="path" class="font-normal text-lg ext-gray-700 flex items-center">
+            {{ title }} <span class="ml-4"><img class="w-auto h-4" :src="arrow" /></span>
         </NuxtLink>
     </div>
 </template>
@@ -9,6 +9,16 @@
 <script>
 export default {
     name: "MoreComponent",
+    props: {
+    path: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    }
+    },
     data() {
         return {
             arrow: require('../assets/arrowright.png'),
