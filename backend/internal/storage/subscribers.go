afb84d55bd817b5dd734605da6aa70b65c918093
Aser Dipanda <aserdipanda@gmail.com>
2023-04-07 14:19:35 +0200

    rename const SubscribersTableName to JobSubscribersTableName

--- backend/internal/storage/subscribers.go
+++ backend/internal/storage/subscribers.go
@@ -5,21 +5,21 @@ import (
 	"gorm.io/gorm"
 )
 
-const SubscribersTableName = "job_subscribers"
+const JobSubscribersTableName = "job_subscribers"
 
 func (db DB) PostSubscribers(query v1beta.SubscribersPostQuery) (*v1beta.Subscribers, error) {
 	subscriber := new(v1beta.Subscribers)
 
 	err := db.c.Transaction(func(tx *gorm.DB) error {
 		//Check if the subscriber email already exist
-		err := tx.Table(SubscribersTableName).Where("email = ?", query.Email).First(subscriber).Error
+		err := tx.Table(JobSubscribersTableName).Where("email = ?", query.Email).First(subscriber).Error
 		if err == nil {
 			return nil
 		}
 
 		subscriber.Email = query.Email
 
-		err = tx.Table(SubscribersTableName).Create(subscriber).Error
+		err = tx.Table(JobSubscribersTableName).Create(subscriber).Error
 		if err != nil {
 			return err
 		}
