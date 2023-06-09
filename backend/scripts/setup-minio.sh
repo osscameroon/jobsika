3d471fdc4be7f484fb1e69386ef15b21861c6282
Aser <aserdipanda@gmail.com>
2023-06-09 16:53:49 +0100

    fix: minio not starting on rootless env

--- backend/scripts/setup-minio.sh
+++ backend/scripts/setup-minio.sh
@@ -8,7 +8,9 @@ ENV_FILE=/api/.docker-env
 
 #install minio client
 if [[ -z "$(which mc)" ]]; then
-	go install github.com/minio/mc@latest
+	wget -O mc https://dl.min.io/client/mc/release/linux-amd64/mc
+	chmod +x mc
+	mv mc /usr/local/bin/mc
 fi
 
 export $(grep -v '#.*' $ENV_FILE | xargs)
