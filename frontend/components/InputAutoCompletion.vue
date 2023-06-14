34760f98207a855a16ccae3a9b9a0a99bdff272f
Abba Sali Aboubakar Mamate <abbasalimokolo@gmail.com>
2023-06-14 08:10:14 +0100

    feat: yarn lint fix

--- frontend/components/InputAutoCompletion.vue
+++ frontend/components/InputAutoCompletion.vue
@@ -1,13 +1,27 @@
 <template>
   <div class="site__input w-full">
-    <p class="text-xs md:text-sm font-bold" style="color: #b1b1b1; font-family: 'Inter', sans-serif">
+    <p
+      class="text-xs md:text-sm font-bold"
+      style="color: #b1b1b1; font-family: 'Inter', sans-serif"
+    >
       {{ title }}
     </p>
-    <input v-model="name" type="text" class="site__input-field border-none mt-3 w-full rounded-md mb-8 md:mb-16"
-      :style="myStyle" />
-    <ul v-if="opened"
-      class="h-20 md:h-32 overflow-y-scroll rounded-lg -mt-3 md:-mt-14 mb-2 md:mb-4 cursor-pointer bg-white">
-      <li v-for="(elem, index) in filteredData" :key="index" class="shadow-sm py-2 px-4" @click="setResult(elem)">
+    <input
+      v-model="name"
+      type="text"
+      class="site__input-field border-none mt-3 w-full rounded-md mb-8 md:mb-16"
+      :style="myStyle"
+    />
+    <ul
+      v-if="opened"
+      class="h-20 md:h-32 overflow-y-scroll rounded-lg -mt-3 md:-mt-14 mb-2 md:mb-4 cursor-pointer bg-white"
+    >
+      <li
+        v-for="(elem, index) in filteredData"
+        :key="index"
+        class="shadow-sm py-2 px-4"
+        @click="setResult(elem)"
+      >
         {{ elem }}
       </li>
     </ul>
@@ -55,7 +69,7 @@ export default {
       return this.filteredData.length > 0
     },
   },
-  created() { },
+  created() {},
   methods: {
     setResult(result) {
       this.name = result
